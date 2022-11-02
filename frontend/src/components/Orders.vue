35113c09b5f4684c891ccf82d4c5c0ded340e0d3
full-owl <112518514+full-owl@users.noreply.github.com>
2022-11-02 15:48:28 -0500

    Discount orders view

---
+++ frontend/src/components/Orders.vue
@@ -0,0 +1,10 @@
+
+<template>
+    <h1>Sides</h1>
+    <button>White Rice</button>
+    <button>Brown Rice</button>
+
+    <h1>Entrees</h1>
+    <button>Chicken</button>
+    <button>Beef</button>
+</template>
