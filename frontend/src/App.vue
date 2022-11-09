9317f579b2f76a957566050762f5a6e12f69b464
full-owl <112518514+full-owl@users.noreply.github.com>
2022-11-08 18:50:08 -0600

    Move MenuItems to from App.js

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,34 +1,13 @@
 <template>
   <v-app>
     <v-main>
-      <!-- <p>Bowls</p>
-      <ButtonTable :items="bowls"/>
-      <br>
-      <p>Entrees</p>
-      <ButtonTable :items="entrees"/>
-      <br>
-      <p>Sides</p>
-      <ButtonTable :items="sides"/>
-      <br>
-      <p>Appetizers</p>
-      <ButtonTable :items="appetizers"/>
-      <br>
-      <p>Drinks</p>
-      <ButtonTable :items="drinks"/>
-      <br>
-       -->
       <MenuItems/>
-      <br>
-
     </v-main>
   </v-app>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
-//import ButtonTable from './components/ButtonTable.vue';
 import MenuItems from './components/MenuItems.vue';
-//import consts from './consts';
 
 
 export default {
@@ -36,41 +15,6 @@ export default {
 
   components: {
     MenuItems,
-    //ButtonTable
   },
-
-  data() {
-    return {
-      entrees: [],
-      appetizers: [],
-      drinks:[],
-      sides:[],
-      bowls:[], // need to implement
-    }
-  },
-  /* methods: {
-    // returns array of items of food type
-    async fetchItems(foodtype) {
-      // connection from backend folder.. made need to change port if doing on web hosting
-      const res = await fetch(`${consts.backend_url}/items/${foodtype}`);
-
-      const data = await res.json();
-      return data;
-    },
-    async fetchCombos() {
-      const res = await fetch(`${consts.backend_url}/combosizes`);
-
-      const data = await res.json();
-      return data;
-    }
-  },
-  async created() {
-
-    this.entrees = await this.fetchItems("entree");
-    this.appetizers = await this.fetchItems("appetizer");
-    this.drinks = await this.fetchItems("drink");
-    this.sides = await this.fetchItems("side");
-    this.bowls = await this.fetchCombos();
-  } */
 }
 </script>
