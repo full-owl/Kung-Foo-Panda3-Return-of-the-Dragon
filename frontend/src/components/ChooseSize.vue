c84789dc43648ea866332de20fc3b321f9d2e614
full-owl <112518514+full-owl@users.noreply.github.com>
2022-11-21 21:48:20 -0600

    Format Document

--- frontend/src/components/ChooseSize.vue
+++ frontend/src/components/ChooseSize.vue
@@ -1,9 +1,6 @@
 <template>
   <!-- TODO: style like bootstrap -->
-  <v-dialog
-      max-width="500"
-    v-model="dialog"
-  >
+  <v-dialog max-width="500" v-model="dialog">
     <v-card :title="'Choose Size for ' + item.name">
       <v-card-text>
         <v-btn-toggle v-model="selected" shaped mandatory>
@@ -13,65 +10,73 @@
         </v-btn-toggle>
       </v-card-text>
       <v-card-actions>
-        <button type="button" class="btn btn-secondary" @click="dialog = false">Cancel</button>
-        <button type="button" class="btn btn-primary" @click="addOrderToReciept">Add</button>
+        <button type="button" class="btn btn-secondary" @click="dialog = false">
+          Cancel
+        </button>
+        <button
+          type="button"
+          class="btn btn-primary"
+          @click="addOrderToReciept"
+        >
+          Add
+        </button>
       </v-card-actions>
     </v-card>
   </v-dialog>
 </template>
 
 <script>
-import consts from '../consts.js';
+import consts from "../consts.js";
 
 export default {
-    props: {
-      receiptTable: Object,
-    },
-    data() {
-        return {
-            dialog: false,
-            item: {},
-            selected: 0,
-            mealsizes: {},
-        }
-    },
-    async created() {
-      // Get sizes from backend
-      const res = await fetch(`${consts.backend_url}/mealsizes`);
-      const mealsizes = await res.json();
-      // Convert from rows to a Map grouped by foodtype like sides, entrees, etc.
-      this.mealsizes = mealsizes.reduce((group,mealsize) => {
-        console.log(mealsize);
-        if(mealsize.foodtype == 'combo') {
-          return group;
-        }
-        if(["small","medium","large"].includes(mealsize.name)) {
-          group[mealsize.foodtype] = group[mealsize.foodtype] ?? [];
-          group[mealsize.foodtype].push(mealsize);
-        }
+  props: {
+    receiptTable: Object,
+  },
+  data() {
+    return {
+      dialog: false,
+      item: {},
+      selected: 0,
+      mealsizes: {},
+    };
+  },
+  async created() {
+    // Get sizes from backend
+    const res = await fetch(`${consts.backend_url}/mealsizes`);
+    const mealsizes = await res.json();
+    // Convert from rows to a Map grouped by foodtype like sides, entrees, etc.
+    this.mealsizes = mealsizes.reduce((group, mealsize) => {
+      console.log(mealsize);
+      if (mealsize.foodtype == "combo") {
         return group;
-      }, {}); 
+      }
+      if (["small", "medium", "large"].includes(mealsize.name)) {
+        group[mealsize.foodtype] = group[mealsize.foodtype] ?? [];
+        group[mealsize.foodtype].push(mealsize);
+      }
+      return group;
+    }, {});
+  },
+  methods: {
+    show(item) {
+      this.item = item;
+      this.dialog = true;
+      return this.dialog;
     },
-    methods: {
-        show(item) {
-          this.item = item;
-          this.dialog = true;
-          return this.dialog;
-        },
-        addOrderToReciept() {
-          this.dialog = false;
-          let type = this.mealsizes[this.item.foodtype][this.selected];
-          this.$emit('addOrder', type, [this.item]);
-        }
-    }
-}
+    addOrderToReciept() {
+      this.dialog = false;
+      let type = this.mealsizes[this.item.foodtype][this.selected];
+      this.$emit("addOrder", type, [this.item]);
+    },
+  },
+};
 </script>
 
 <!-- TODO: figure out how to not duplicate this for every component -->
 <style scoped>
-    .btn {
-        border-radius: 0%;
-        border-color: aliceblue;
-        color: aliceblue;
-    }
+.btn {
+  border-radius: 0%;
+  border-color: aliceblue;
+  color: aliceblue;
+}
 </style>
