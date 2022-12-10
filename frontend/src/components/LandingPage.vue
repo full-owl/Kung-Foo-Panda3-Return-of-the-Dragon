fb2bd685b2b6efe3feef78e47f779bcc6909cd82
Naimur Rahman <naimurrah01@tamu.edu>
2022-12-10 03:02:32 -0600

    set up all of oauth2 but not initializing

--- frontend/src/components/LandingPage.vue
+++ frontend/src/components/LandingPage.vue
@@ -9,18 +9,57 @@
         <div class ="map">
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3433.770986512828!2d-96.3435775849168!3d30.612222181679375!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86468398ef48705d%3A0xb75709775b0df8b1!2sPanda%20Express%20-%20MSC!5e0!3m2!1sen!2sus!4v1670015412837!5m2!1sen!2sus" width="400" height="300" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> 
         </div>
+        <div class="login">
+            <h1>Is Init: {{Vue3GoogleOauth.isInit}}</h1>
+            <h1>Is Authorized: {{Vue3GoogleOauth.isAuthorized}}</h1>
+            <button class='btn btn-primary' @click='handleSignIn' :disabled='!Vue3GoogleOauth.isInit || Vue3GoogleOauth.isAuthorized'>Sign In</button>
+            <button class='btn btn-primary' @click='handleSignOut' :disabled='!Vue3GoogleOauth.isAuthorized'>Sign Out</button>
+        </div>
     </div>
     
 </template>
 
 <script>
-
+import {inject} from 'vue';
 export default{
     name: 'LandingPage',
 
     components: {
         
-    }
+    },
+    props: {
+
+    },
+    methods: {
+        async handleSignIn() {
+            try {
+                const googleUser = await this.$gAuth.signIn();
+                // console.log(this.$gAuth.signIn);
+                if (!googleUser) {
+                    return null;
+                }
+                 this.user = googleUser.getBasicProfile().getEmail();
+            } catch (error) {
+                console.log(error);
+                return null;
+            }
+        },
+        async handleSignOut() {
+            try {
+                await this.$gAuth.signOut();
+                // console.log(this.$gAuth.signOut);
+                this.user = '';
+            } catch (error) {
+                console.log(error);
+            }
+        },
+  },
+    setup() {
+        const Vue3GoogleOauth = inject('Vue3GoogleOauth');
+        return {
+            Vue3GoogleOauth,
+        };
+  },
 }
 </script>
 
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,13 +6,19 @@ import vuetify from './plugins/vuetify'
 // import MenuItems from './components/MenuItems'
 // import RecieptTable from './components/RecieptTable'
 import router from './router' 
-
+import gAuthPlugin from 'vue3-google-oauth2';
 // loadFonts()
+const app = createApp(App).use(router);
 
-createApp(App)
-  .use(router)
-  .use(vuetify)
+let gauthClientId = '752431398514-7sg211luu4kccppqknilib6dupjq49i4.apps.googleusercontent.com';
+app.use(vuetify);
+
+app.use(gAuthPlugin, {
+  clientId: gauthClientId,
+  scope: 'email',
+  prompt: 'consent',
+});
   // .use('MyModal', MyModal)
   // .use('MenuItems',MenuItems)
   // .use('RecieptTable', RecieptTable)
-  .mount('#app')
+app.mount('#app');
