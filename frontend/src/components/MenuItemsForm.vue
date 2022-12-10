2959968e4ed956ec33f69129c9ab05c59766010c
full-owl <112518514+full-owl@users.noreply.github.com>
2022-12-10 14:22:55 -0600

    Better add menu items form

--- frontend/src/components/MenuItemsForm.vue
+++ frontend/src/components/MenuItemsForm.vue
@@ -1,5 +1,5 @@
 <template>
-    <form @submit.prevent="handleSubmit" class="row g-3">
+    <form @submit.prevent="handleSubmit" class="container g-3">
         <label class="col-md-6">Name: <input type="text" class="form-control" v-model="data.name" /></label>
         <label class="col-md-6">Food type:
             <select class="form-select" v-model="data.foodtype">
@@ -8,19 +8,27 @@
             </select>
         </label>
         <label>Description: <textarea class="form-control" v-model="data.description" /></label>
-        <label class="col-md-9">Add Ingredient:
-            <select v-model="selectedIngredient" class="form-select">
-                <option :value="null" selected>Choose an ingredient</option>
-                <option v-for="ingredient in ingredients" :key="ingredient.id" :value="ingredient">{{ ingredient.ingredient }}</option>
-            </select>
-        </label>
-        <div class="col-md-1">
-            <button type="button" class="btn btn-secondary" @click="addIngredient">Add ingredient</button>
+        <div class="row">
+            <label class="col-sm-auto">Add Ingredient:
+                <select v-model="selectedIngredient" class="form-select">
+                    <option :value="null" selected>Choose an ingredient</option>
+                    <option v-for="ingredient in ingredients" :key="ingredient.id" :value="ingredient">{{ ingredient.ingredient }}</option>
+                </select>
+            </label>
+            <label class="col-sm">Porportion:
+                <input v-model.number="ingredientProportion" type="number" class="form-control" min="0.1" max="1" />
+            </label>
+            <div class="col-sm">
+                <button type="button" class="btn btn-secondary" @click="addIngredient">Add ingredient</button>
+            </div>
         </div>
         <div>
-            <label v-for="(x, index) in data.ingredients" :key="x.id">{{ x.ingredient }}
-                <input v-model.number="data.ingredients[index].porportion" type="number" class="form-control" min="0.1" max="1" step="0.1" />
-            </label>
+            <div v-for="(x, index) in data.ingredients" :key="x.id">
+                <label>{{ x.ingredient }}
+                    <input v-model.number="data.ingredients[index].porportion" type="number" class="form-control" min="0.1" max="1" step="0.1" />
+                </label>
+                <button type="button" class="btn btn-secondary" @click="removeIngredient(index)">X</button>
+            </div >
         </div>
         <button type="submit" class="btn btn-primary" :disabled="data.ingredients.length == 0">Add</button>
     </form>
@@ -35,6 +43,7 @@ export default {
             foodTypes: ["entree", "appetizer", "drink", "side"],
             ingredients: [],
             selectedIngredient: null,
+            ingredientProportion: 0.1,
             data: {
                 name: "",
                 foodtype: "",
@@ -51,15 +60,19 @@ export default {
     },
     methods: {
         addIngredient() {
-            // console.log(this.ingredients);
             if(this.selectedIngredient && !this.data.ingredients.includes(this.selectedIngredient)) {
+                this.selectedIngredient.porportion = this.ingredientProportion;
                 this.data.ingredients.push(this.selectedIngredient);
+                
+                // reset ingredient selector
+                this.selectedIngredient = null;
+                this.ingredientProportion = 0.1;
             } else {
                 console.error("Invalid selected ingredient", this.selectedIngredient);
             }
         },
         handleSubmit() {
-            // TODO: error handling, and connecting to backend
+            // TODO: error handling
             fetch(`${consts.backend_url}/menuitem`, {
                 headers: {
                     'Accept': 'application/json',
@@ -71,7 +84,9 @@ export default {
             .then(res => res.json())
             .then(data => console.log("Response: ", data))
             .catch(err => console.error(err));
-            // console.error("handeled");
+        },
+        removeIngredient(index) {
+            this.data.ingredients.splice(index, 1);
         }
     }
 }
